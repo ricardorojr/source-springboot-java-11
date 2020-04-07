3644d38de9b5a0d66f2cd52514473429476972b4
Ricardo Rodrigues <ricardorodriguesjr87@gmail.com>
2020-04-07 18:07:53 -0300

    lista de items criados e associasoes

---
+++ src/main/java/com/ricardo/course/repositories/OrderItemRepository.java
@@ -0,0 +1,11 @@
+package com.ricardo.course.repositories;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ricardo.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+
+}
