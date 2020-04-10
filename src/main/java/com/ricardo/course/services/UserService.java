fc52f4d526d4f2b00107fcee7d4a9b9cc67ffe3d
Ricardo Rodrigues <ricardorodriguesjr87@gmail.com>
2020-04-10 12:38:54 -0300

    update tratamento

--- src/main/java/com/ricardo/course/services/UserService.java
+++ src/main/java/com/ricardo/course/services/UserService.java
@@ -3,6 +3,8 @@ package com.ricardo.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -33,19 +35,23 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		try { 
-		repository.deleteById(id);
-		}catch (EmptyResultDataAccessException e) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
-		}catch (DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DataBaseException(e.getMessage());
 		}
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getOne(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
